c4762814b9476e1fbf05c4e39c4f2ecaa49ff536
Vitor <vitornunes2747@gmail.com>
2023-08-30 16:02:02 -0300

    TypeOrm configurado

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,52 +1,49 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
-// import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-    private coffees: Coffee[] = [
-        {
-            id: 1,
-            name: 'Shipwreck Roast',
-            brand: 'Buddy Brew',
-            flavors: ['chocolate', 'vanilla']
-        }
-    ]
+    constructor(
+        @InjectRepository(Coffee)
+        private readonly coffeeRepository: Repository<Coffee>
+    ) {}
 
     findAll() {
-        return this.coffees;
+        return this.coffeeRepository.find();
     }
 
-    findOne(id: string) {
-        const coffee = this.coffees.find(item => item.id === +id);
+    async findOne(id: string) {
+        const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
         if (!coffee){
             throw new NotFoundException(`Coffee #${id} not found.`);
         }
         return coffee;
     }
 
-    create(createCoffeDto: any) {
-        // console.log(createCoffeDto instanceof CreateCoffeeDto);
-        const originalCoffees = this.coffees;
-        const size = this.coffees.length;
-        
-        this.coffees.push({ id: originalCoffees[size - 1].id + 1 , ...createCoffeDto });
+    create(createCoffeDto: CreateCoffeeDto) {
+        const coffee = this.coffeeRepository.create(createCoffeDto);
+        return this.coffeeRepository.save(coffee);
     }
 
-    update(id: string, updateCoffeeDto: any) {
-        const existingCoffee = this.findOne(id);
-        if (existingCoffee) {
-            const coffeeIndex = this.coffees.indexOf(existingCoffee);
-            delete updateCoffeeDto.id;
+    async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+        const coffee = await this.coffeeRepository.preload({
+            id: +id,
+            ...updateCoffeeDto
+        });
 
-            this.coffees[coffeeIndex] = { id: Number(id), ...existingCoffee, ...updateCoffeeDto };
+        if (!coffee) {
+            throw new NotFoundException(`Coffee #${id} not found.`);
         }
+
+        return this.coffeeRepository.save(coffee);
     }
 
-    remove(id: string) {
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-        if (coffeeIndex >= 0) {
-            this.coffees.splice(coffeeIndex, 1);
-        }
+    async remove(id: string) {
+        const coffee = await this.findOne(id);
+        return this.coffeeRepository.remove(coffee);
     }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()  // Ao passar parametro,a tabela é renomeada. Ex.: ('coffees') 
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavors: string[];
 }
